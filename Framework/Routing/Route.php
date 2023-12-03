a1c95b75102b06a6e78a843d5839cad82d3e607a
Abdouni Abdelkarim <abdounikarim@gmail.com>
2023-12-03 15:28:49 +0100

    Slugger

--- Framework/Routing/Route.php
+++ Framework/Routing/Route.php
@@ -129,23 +129,7 @@ class Route
 
     public function removeSpecialChars(string $parameter): string
     {
-        // TODO - handle other chars ? (show generate method if needed)
         return preg_replace('#/|{|}#', '', $parameter);
-    }
-
-    public function generate(string $string): string
-    {
-        $string = preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
-        $string = preg_replace('#à|á|â|ã|ä|å#', 'a', $string);
-        $string = preg_replace('#ç#', 'c', $string);
-        $string = preg_replace('#è|é|ê|ë#', 'e', $string);
-        $string = preg_replace('#ì|í|î|ï#', 'i', $string);
-        $string = preg_replace('#ð|ò|ó|ô|õ|ö#', 'o', $string);
-        $string = preg_replace('#ù|ú|û|ü#', 'u', $string);
-        $string = preg_replace('#ý|ÿ#', 'y', $string);
-        $string = preg_replace('#|_|:|,|\'|"#', '', $string);
-
-        return $string;
     }
 
     public function updatePath(): void
--- Framework/Slugger/Slugger.php
+++ Framework/Slugger/Slugger.php
@@ -8,7 +8,14 @@ final class Slugger implements SluggerInterface
 {
     public function slug(string $string, string $separator = '-'): string
     {
-        // TODO - handle special chars like é, è, ç, etc.
-        return preg_replace('/\s+/', $separator, mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
+        $string = preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
+        $string = preg_replace('#à|á|â|ã|ä|å#', 'a', $string);
+        $string = preg_replace('#ç#', 'c', $string);
+        $string = preg_replace('#è|é|ê|ë#', 'e', $string);
+        $string = preg_replace('#ì|í|î|ï#', 'i', $string);
+        $string = preg_replace('#ð|ò|ó|ô|õ|ö#', 'o', $string);
+        $string = preg_replace('#ù|ú|û|ü#', 'u', $string);
+        $string = preg_replace('#ý|ÿ#', 'y', $string);
+        return preg_replace('#|_|:|,|\'|"#', '', $string);
     }
 }
